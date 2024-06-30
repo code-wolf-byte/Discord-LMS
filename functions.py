2f116ed3cccbfce886491c045ec1cbcd1bdbd4fb
code-wolf-byte <hackerstark37@gmail.com>
2024-06-30 10:02:55 +0000

    archival

--- functions.py
+++ functions.py
@@ -5,4 +5,4 @@ from main import Utilities
 print(discord.__version__)
 client = commands.Bot(command_prefix='!',)
 client.add_cog(Utilities(client))
-client.run("ODM2MzcwODYxNTQ4MTA5ODk0.YIdBEA.3jW1_FAMPFePnMvcj9eVaFU2d8U")
+client.run("")
